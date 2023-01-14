1c9ee8d12b6a8375687c2cd4a37b9a916a9a99ac
Afzal Hussain <azzu01122@gmail.com>
2023-01-14 10:54:20 +0530

    all components working great. final commit

--- Api/views.py
+++ Api/views.py
@@ -6,9 +6,10 @@ from rest_framework.response import Response
 # from django.shortcuts import redirect
 from rest_framework import status
 from django.http import HttpResponseRedirect
-from django.http import JsonResponse
 
 
+# This view returns property details.
+'''If city is mentioned then it filter properties for these cities otherwise fatches all the data''' 
 @api_view(["GET"])
 def FatchPropertyDetails(request):
     if request.method == "GET":
@@ -16,7 +17,6 @@ def FatchPropertyDetails(request):
         if city_data is not None:
             prop_dtl = PropertyDetail.objects.filter(city=city_data)
             serializer = PropertyDetailSerializer(prop_dtl, many=True)
-            print(serializer)
             return Response(serializer.data)
 
         prop_dtl = PropertyDetail.objects.all()
@@ -24,18 +24,23 @@ def FatchPropertyDetails(request):
         return Response(serializer.data, status=status.HTTP_200_OK)
 
 
+'''this view returns all the property details 
+after creation of new property.'''
 @api_view(["POST"])
 def CreateNewProperty(request):
     if request.method == "POST":
-        state = request.data.get("state")
         serializer = PropertyDetailSerializer(data = request.data)
         if serializer.is_valid():
-            # serializer.save()
+            serializer.save()
             return HttpResponseRedirect(redirect_to="http://127.0.0.1:8000/fetch_property_details/")
             # return redirect("fetchproperty")
         return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
 
 
+'''This view updates the existing details and returns new updated list. we need to provide every field for update.'''
+'''we also can partially update data for that we need following changes in UpdatePropertyDetails view
+1 ---> @api_view(["PATCH"])   put this decorator in view
+2 ---> partial=True    add this in PropertyDetailSerializer'''
 @api_view(["PUT"])
 def UpdatePropertyDetails(request):
     if request.method == "PUT":
@@ -43,7 +48,6 @@ def UpdatePropertyDetails(request):
         if id is not None:
             prop_dtl = PropertyDetail.objects.get(id=id)
             serializer = PropertyDetailSerializer(prop_dtl, data=request.data)
-            print(serializer)
             if serializer.is_valid():
                 serializer.save()
                 return HttpResponseRedirect(redirect_to="http://127.0.0.1:8000/fetch_property_details/")
@@ -51,40 +55,26 @@ def UpdatePropertyDetails(request):
         return Response(res, status=status.HTTP_400_BAD_REQUEST)
 
 
-STATE_IDS = (
-    ("an", "andaman and nicobar islands"),
-    ("ap", "andhra pradesh"),
-    ("ar", "arunachal pradesh"),
-    ("uk", "uttarakhand"),
-)
-
+'''This view returns the list of all the cities those are belongs to
+the provided state name'''
 @api_view(["GET"])
 def FindCitiesByState(request):
     if request.method == "GET":
-        state_id = request.data.get("state_id")
         state_name = request.data.get("state")
-        print(state_id)
-        print(state_name)
-        cities = []
-        for tup in STATE_IDS:
-            if state_id == tup[0]:
-                state_name = tup[1]
-                prop_dtl = list(PropertyDetail.objects.filter(state_id=state_name))
-                for prop in prop_dtl:
-                    cities.append(prop.city)
-                break
-        # serializer = PropertyDetailSerializer(prop_dtl, many=True)
-        return JsonResponse(cities, safe=False)
-
-        # if state_name is not None:
-        #     prop_dtl = PropertyDetail.objects.filter(state=state_name)
-        #     find_serializer = PropertyDetailSerializer(prop_dtl, many=True)
-        #     return Response(find_serializer.data)
-
-        # else:
-        # #     return Response({"msg":"please provide state id or state name."})
+        if state_name is not None:
+            cities_list = []
+            prop_dtl = PropertyDetail.objects.filter(state=state_name)
+            serializer = PropertyDetailSerializer(prop_dtl, many=True)
+            for dict in serializer.data:
+                cities_list.append(dict["city"])
+            res = {"cities":cities_list}
+            return Response(res, status=status.HTTP_404_NOT_FOUND)
+        res = {"state":"Please enter state name"}
+        return Response(res, status=status.HTTP_404_NOT_FOUND)
 
 
+'''This view returns the list of properties those are belongs to the same cities
+with mentioned property'''
 @api_view(["GET"])
 def FindSimilarProperties(request):
     if request.method == "GET":
